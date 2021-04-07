97657eebebed5c00045260df3956c90ff1559ca8
Wty-989 <1010945201@qq.com>
2021-04-07 15:12:31 +0800

    初识c语言

---
+++ test_3_31/test_3_31/test.c
@@ -0,0 +1,16 @@
+//写代码
+//1.写出主函数（main函数）
+//100-500代码
+//如何执行？ - C语言是从主函数的第一行开始执行的
+//所有c语言都有main函数 -入口
+
+#include <stdio.h>
+
+int main()
+{
+	printf("比特\n");
+	printf("%d\n", 100);//打印一个整数 -%d
+	printf("%d\n",sizeof(char))； //sizeof -关键字 -操作符 - 计算类似或看变量所占空间大小 - 单位：字节
+
+	return 0;
+}
